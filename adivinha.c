14933d2147c0215304ebdfa17523ca3ee707aa29
Willian Scappini Britez <74613589+williansca@users.noreply.github.com>
2022-09-05 15:05:27 -0300

    While Loop

--- adivinha.c
+++ adivinha.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /*constante numero de tentativas para usar como limite no for*/
-#define NUMERO_DE_TENTATIVAS 3
+//#define NUMERO_DE_TENTATIVAS 3
 
 int main () {
 	
@@ -12,38 +12,55 @@ int main () {
 	int numero = 9;
 	int chute;
     
+    int ganhou = 0;
+    int tentativas = 1;
     
-    /*Looping*/
-    for(int i = 1; i <= NUMERO_DE_TENTATIVAS; i++) {
+    
+    /*for Looping contado*/
+    //for(int i = 1; i <= NUMERO_DE_TENTATIVAS; i++) 
+    while (ganhou == 0){
         
-        printf("Tentativa %d de %d\n", i, NUMERO_DE_TENTATIVAS);
+        //printf("Tentativa %d de %d\n", i, NUMERO_DE_TENTATIVAS);
+        printf("Tentativa numero: %d\n", tentativas);
         
         printf("Chute um numero de 1 a 10: ");
         scanf("%d", &chute);
         printf("Seu chute foi: %d\n", chute);
         
-        int acertou = (chute == numero);
+        if(chute < 0) {
+            printf("Somente numeros positivos\n");
+            //i--;
+            /*para a execucao do bloco de codigo mas o for continua*/
+            continue;
+        }
         
-        printf("Acertou: %d\n", acertou);
+        int acertou = (chute == numero);
+        int maior = chute > numero;
+        //int menor = chute < numero;
+        
+        /*printing boolean variable that shows 1 for true and 0 for false*/
+        //printf("Acertou: %d\n", acertou);
         
         if (acertou) {
-            printf("Voce acertou! Parabens!\n");
+            printf("Voce acertou! Parabens!\n\n");
             
-            /*se acertou o codigo para*/
-            break;
-        }
-        else {
-            /* a variavel maior existe somente no escopo do else */
-            int maior = chute > numero;
+            //break;
+            ganhou = 1;
             
-            if( maior) {
-                printf("Voce errou! Seu chute foi maior do que o numero secreto\n");
-            } else  {
-                printf("Voce errou! Seu chute foi menor do que o numero secreto\n");
-            } 
-        }
+        } else if(maior){ 
+            /* a variavel maior existe somente no escopo do else 
+            int maior = chute > numero;*/
+            printf("Voce errou! Seu chute foi maior do que o numero secreto\n\n");
+            
+        } else  {
+            printf("Voce errou! Seu chute foi menor do que o numero secreto\n\n");
+        } 
         
+        tentativas++;
     }
     
     printf("Fim de jogo\n");
+    printf("Voce acertou em %d tentativas", tentativas-1);
 }
+    
+    
